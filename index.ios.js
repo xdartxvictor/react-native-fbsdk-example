719218c6e0226b1083ceacecfda644375aa09567
victor <xdartxvictor@gmail.com>
2017-06-30 00:15:46 -0500

    [add] facebook SDK

--- index.ios.js
+++ index.ios.js
@@ -11,6 +11,10 @@ import {
   Text,
   View
 } from 'react-native';
+const FBSDK = require('react-native-fbsdk')
+const {
+  LoginButton
+} = FBSDK
 
 export default class FacebookSDKLogin extends Component {
   render() {
@@ -26,6 +30,21 @@ export default class FacebookSDKLogin extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error)
+              } else if (result.isCancelled) {
+                alert("Login was cancelled")
+              } else {
+                alert("Login was successful with permissions: " + result.grantedPermissions)
+              }
+            }
+          }
+          onLogoutFinished={() => alert("User logged out")}/>
       </View>
     );
   }
